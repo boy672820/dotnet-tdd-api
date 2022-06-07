19bea008597e18733c213e9094283c11c32aef07
seonzoo <boy672820@gmail.com>
2022-06-08 01:39:12 +0900

    Before TDD

--- pizza-server.test/Controllers/PizzaController/TestCreate.cs
+++ pizza-server.test/Controllers/PizzaController/TestCreate.cs
@@ -1,47 +1,5 @@
-using PizzaSchool.Controllers;
-using PizzaSchool.Models;
-using Microsoft.AspNetCore.Mvc;
-// using FluentAssertions;
-// using Xunit;
-
 namespace PizzaSchool.UnitTests.Systems.Controllers;
 
 public class TestCreate
 {
-  public TestCreate()
-  {
-    this.sut = new PizzaController();
-  }
-
-  private PizzaController sut;
-
-  [Fact]
-  public void SutCheckForDuplicateName()
-  {
-    Pizza newPizza = new Pizza { Name = "Hawaiian", IsGlutenFree = true };
-
-    var actual = this.sut.Create(newPizza);
-
-    Assert.IsType<BadRequestResult>(actual);
-  }
-
-  [Fact]
-  public void SutCorrectlyTrimsEmptyValue()
-  {
-    Pizza newPizza = new Pizza { Name = "", IsGlutenFree = true };
-
-    var actual = this.sut.Create(newPizza);
-
-    Assert.IsType<BadRequestResult>(actual);
-  }
-
-  [Fact]
-  public void IfAllergiesAreNotEmptyCheckValues()
-  {
-    Pizza newPizza = new Pizza { Name = "Test", IsGlutenFree = true, Allergies = new string[] { "Milk", "Egg", "Peanut" } };
-
-    var actual = this.sut.Create(newPizza);
-
-    Assert.IsType<BadRequestResult>(actual);
-  }
 }
--- pizza-server/Controllers/PizzaController.cs
+++ pizza-server/Controllers/PizzaController.cs
@@ -31,67 +31,6 @@ public class PizzaController : ControllerBase
   [HttpPost]
   public IActionResult Create(Pizza newPizza)
   {
-    // 검증되지 않은 코드
-    var pizza = PizzaService.GetByName(newPizza.Name);
-
-    if (pizza != null)
-    {
-      return BadRequest();
-    }
-
-    // TDD 검증코드
-    if (newPizza.Name == "")
-    {
-      return BadRequest();
-    }
-
-    // TDD 검증코드
-    if (newPizza.Allergies != null)
-    {
-      string[] validAllergies = new string[] { "Milk", "Egg", "Tomato" };
-      bool isAllergies = newPizza.Allergies.All(a => validAllergies.Contains(a));
-
-      if (!isAllergies)
-      {
-        return BadRequest();
-      }
-    }
-
     return CreatedAtAction(nameof(Create), new { id = newPizza.Id }, newPizza);
-  }
-
-  [HttpPut("{id}")]
-  public IActionResult Update(int id, Pizza pizza)
-  {
-    if (id != pizza.Id)
-    {
-      return BadRequest();
-    }
-
-    var existingPizza = PizzaService.Get(id);
-
-    if (existingPizza is null)
-    {
-      return NotFound();
-    }
-
-    PizzaService.Update(pizza);
-
-    return NoContent();
-  }
-
-  [HttpDelete("{id}")]
-  public IActionResult Delete(int id)
-  {
-    var pizza = PizzaService.Get(id);
-
-    if (pizza is null)
-    {
-      return NotFound();
-    }
-
-    PizzaService.Delete(id);
-
-    return NoContent();
   }
 }
